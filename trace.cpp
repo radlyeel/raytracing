174b7c6f0f5997e7feacd69ce03454dbcacbb208
Daryl Lee <daryl@daryllee.com>
2024-01-10 10:09:37 -0700

    6.7 Two spheres

--- trace.cpp
+++ trace.cpp
@@ -2,10 +2,15 @@
 /* References:
  * https://raytracing.github.io/books/RayTracingInOneWeekend.html
  */
+#include "rtweekend.h"
 #include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "hittable.h"
+#include "hittablelist.h"
+#include "sphere.h"
 
+#include <iostream>
+
+#if 0
 double hit_sphere(const point3& center, double radius, const ray& r) {
     // Simplified by replacing b with 2h
     vec3 oc = r.origin() - center;
@@ -20,13 +25,14 @@ double hit_sphere(const point3& center, double radius, const ray& r) {
         return (-h - sqrt(discriminant) ) / a;
     }
 }
+#endif
 
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r,0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
+
     // No hit, just give background color
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
@@ -44,17 +50,20 @@ int main() {
     int image_height = static_cast<int> (image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
-    // Viewport
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * ( static_cast<double>(image_width)/image_height );
+    // World
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // Camera
+    // See Figure 4: Viewport and pixel grid
 
     auto focal_length = 1.0;
     auto camera_center = point3(0, 0, 0);
 
-    // See Figure 4: Viewport and pixel grid
-    
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * ( static_cast<double>(image_width)/image_height );
     auto viewport_u = vec3(viewport_width, 0, 0);
     auto viewport_v = vec3(0, -viewport_height, 0);
     auto pixel_delta_u = viewport_u / image_width;
@@ -66,6 +75,7 @@ int main() {
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
+    // Render
     for (int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
@@ -73,7 +83,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
 
             ray r(camera_center, ray_direction);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
