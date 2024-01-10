0d0bfe7e793690a1d209845ee3ba7b0fc45b8d36
Daryl Lee <daryl@daryllee.com>
2024-01-09 21:40:04 -0700

    Ch05 done -- Red circle

--- trace.cpp
+++ trace.cpp
@@ -6,8 +6,20 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
-    // See notes above Listing 10
+    if (hit_sphere(point3(0,0,-1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
